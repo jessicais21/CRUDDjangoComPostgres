0c28014dbec308a0430782445540d73a6df81932
jessicais21 <jessicalaisis21@gmail.com>
2020-07-04 22:47:43 -0300

    integrando com posgres

--- project/settings.py
+++ project/settings.py
@@ -76,8 +76,14 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+     #   'NAME': os.path.join('clientes', 'clientes'),
+   # 'NAME': os.environ.get('DB_NAME', 'nome_do_banco'),
+        'NAME': 'clientes',
+        'USER': 'postgres',
+        'PASSWORD': '12345',
+        'HOST': '127.0.0.1',
+        'PORT': '5432', # 8000 is default
     }
 }
 
